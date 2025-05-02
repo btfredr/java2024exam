e3fdd6dd791fe123855ec7a2b1bab8aadf4ed57a
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-02 10:10:51 +0200

    task1 init

---
+++ Task1.java
@@ -0,0 +1,21 @@
+/*
+    Task 1. (10 points)
+
+    Write a method that takes a double value as an argument and returns the smallest even integer
+    number greater than or equal to the input number. For example:
+
+    • For input = 25.2, the method returns 26.
+    • For input = 7.0, the method returns 8.
+    • For input = -5.75, the method returns -4.
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int smallestInt(double input) {
+        
+    }
+}
