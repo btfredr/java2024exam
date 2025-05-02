5f21d9740d0ef1b9569577fb5cab2bb2e262680e
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-02 12:50:43 +0200

    Finished task and output

--- Task5.java
+++ Task5.java
@@ -16,21 +16,45 @@
 
 public class Task5 {
     public static void main(String[] args) {
-
+        analyzeString("Java programming 101");
     }
 
-    public void analyzeString(String input) {
+    public static void analyzeString(String input) {
 
         int vowelCount = 0;
         int consonantCount = 0;
         int digitCount = 0;
 
-        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
         char[] consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
+            char lowerC = Character.toLowerCase(c); // Gjør c til liten bokstav
+
+            for(char vowel : vowels) { // Sjekker om det er vokaler i strengen
+                if(lowerC == vowel) {
+                    vowelCount++;
+                }
+            }
+
+            for(char consonant : consonants) { // Sjekker om det er konsonanter i strengen
+                if(lowerC == consonant) {
+                    consonantCount++;
+                }
+            }
+
+            if (Character.isDigit(c)) { // Sjekker om det er tall i strengen
+                digitCount++;
+            }
+
         }
+
+        System.out.println("String information: ");
+        System.out.println("The string contains " + vowelCount + " vowels!");
+        System.out.println("The string contains " + consonantCount + " consonants!");
+        System.out.println("The string is " + input.length() + " characters long!");
+        System.out.println("The string contains " + digitCount + " digits!");
     
     }
 }
