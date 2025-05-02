792a13f094b9de5e819214b8c83a5ba9ecff1d68
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-02 10:30:13 +0200

    task 2 init

---
+++ Task2.java
@@ -0,0 +1,25 @@
+/*
+    Task 2. (15 points)
+
+    Write a Java program that uses nested for loops to print out an inverted pyramid of numbers. The
+    pyramid should have a number of rows equal to the height specified by the user. Each row should
+    display decrementing numbers starting from the row number. For example, if the user specifies a
+    height of 4, the pyramid printed should look like this:
+
+        4 3 2 1
+        3 2 1
+        2 1
+        1
+
+    The program should prompt the user to enter the desired height and output the pyramid
+    accordingly. Each level of the pyramid should have decreasing numbers starting from the row
+    number down to 1
+ */
+
+
+public class Task2 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
