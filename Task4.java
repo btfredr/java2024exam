78ea26fbaab40589735fbd2841f257e051d087b3
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-02 11:47:44 +0200

    task4 init

---
+++ Task4.java
@@ -0,0 +1,18 @@
+/*
+    Task 4 (10 points)
+
+    Write a Java program that reads a distance in kilometers from the user and converts it to miles
+    using the formula: miles = kilometers * 0.621371. For example:
+
+    • E.g. 1: If kilometers is 1, the method returns 0.621371.
+    • E.g. 2: If kilometers is 5, the method returns 3.106855.
+ */
+
+ 
+
+public class Task4 {
+    public static void main(String[] args) {
+
+    }
+
+}
