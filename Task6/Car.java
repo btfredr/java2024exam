5e7de4350e83ccabca28cc702e6d0150a19c75d6
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-05 18:09:31 +0200

    added data fields in car

--- Task6/Car.java
+++ Task6/Car.java
@@ -1,5 +1,27 @@
 package Task6;
 
+/*
+    • Task 6.5: Additional fields in the Car subclass (1 point)
+    o The Car subclass should include:
+    § A field that holds the number of doors.
+    § A field that indicates whether the car is electric.
+
+    • Task 6.6: Constructor for Car (3 points)
+    o The Car class should have an additional constructor that updates the two new fields
+    in addition to the constructor from the superclass.
+
+    • Task 6.7: Methods in Car (3 points)
+    o Add methods getNumberOfDoors and isElectric that read and return the values of
+    the fields doors and electric, respectively.
+
+    • Task 6.8: Override print method in Car (3 points)
+    o Override the print method from the superclass to create a method in the Car class
+    that prints information from all available fields.
+ */
+
 public class Car {
-    
+    private int numOfDoors;
+    private boolean isElectric;
+
+
 }
