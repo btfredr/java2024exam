ba537461b8b6b0767dd605e46fc29fd2d337b6fc
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-05 18:25:58 +0200

    added methods to truck

--- Task6/Truck.java
+++ Task6/Truck.java
@@ -1,5 +1,30 @@
 package Task6;
+/*
+    • Task 6.9: Additional field in the Truck subclass (2 points)
+    o The Truck subclass should contain an additional field that stores the cargo capacity in
+    tons.
 
-public class Truck {
-    
+    • Task 6.10: Polymorphic print methods in Truck (4 points)
+    o Use polymorphism to create two methods in the Truck class to print information
+    about the truck:
+    § One method should print only the model and year.
+    § The other method should print all available information, including the cargo
+    capacity.
+ */
+public class Truck extends Vehicle {
+    private double cargoCapacity;
+
+    public void printModelAndYear() {
+        System.out.println("Truck info: ");
+        System.out.println("Model: " + getModel());
+        System.out.println("Year: " + getYear());
+    }
+
+    public void printAllInfo() {
+        System.out.println("All truck info:");
+        System.out.println("ID: " + getID());
+        System.out.println("Model: " + getModel());
+        System.out.println("Year: " + getYear());
+        System.out.println("Cargo capacity: " + cargoCapacity);
+    }
 }
