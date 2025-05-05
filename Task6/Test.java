1ec12d7729d9bae8cd98d5e7140f5dde349ae511
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-05-05 18:27:03 +0200

    added test class

---
+++ Task6/Test.java
@@ -0,0 +1,18 @@
+package Task6;
+
+/*
+    • Task 6.11: Test class with a main method (5 points)
+    o Create a simple Test class with a main method to create instances of one Car and one
+    Truck and print details from both.
+
+    Pay attention to the use of access modifiers and the readability of the code. During the assessment
+    of this task, special attention will be focused on the ability to implement fundamental principles of
+    object-oriented programming: inheritance, encapsulation, and polymorphism.
+ */
+
+public class Test {
+    
+    public static void main(String[] args) {
+        
+    }
+}
